cb0bcdad7731e2c8de557553eecd8b23333ad900
Jackster001 <wlin041@gmail.com>
2017-08-17 16:58:40 -0400

    Add files via upload

---
+++ js/generalscript.js
@@ -0,0 +1,41 @@
+"use strict";
+
+//    This sets an href to the element
+//    for onclicks
+//    Parameters: String location.
+//    Return: none.
+function locationLink(locationstring) {
+    location.href = locationstring;
+}
+
+//    This function searches all elements with classname, makes them invisible,
+//    except makes visible the element with the idname
+//    Parameters: String classname, String idname
+//    Return: none
+function swap(classname, idname) {
+    var sameclass = document.getElementsByClassName(classname);
+    for (var i = 0; i < sameclass.length; i++) {
+        if (sameclass[i].id == idname) {
+            sameclass[i].style.display = 'block';
+        } else {
+            sameclass[i].style.display = 'none';
+        }
+    }
+}
+// this is for clicable buttons on buttom
+$(".myBox").click(function() {
+  window.location = $(this).find("a").attr("href"); 
+  return false;
+});
+
+//this is for the carousel
+$('.carousel').carousel({
+        interval: 8000
+});
+
+function scroll(val)
+{
+    //alert(document.getElementById(val));
+    document.getElementById(val).click()
+}
+
